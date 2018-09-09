bb78d171a17220ebab83e5a4f03000982e016af4
Eistern <zulind@inbox.ru>
2018-09-09 15:46:26 +0700

    Fixed minor bugs

--- Lab0b/main.cpp
+++ Lab0b/main.cpp
@@ -8,11 +8,11 @@
 using namespace std;
 
 string TidyString(string input) {
-    int to = input.size();
+    int to = input.length();
     for (unsigned int i = 0; i < to; ++i)
-        while (i < to && strchr(",.;:()!?'", input.at(i)) != nullptr) {
+        while (i < to && strchr(",.;:()!?'\n", input.at(i)) != nullptr) {
             input.erase(input.begin() + i);
-            to = input.size();
+            to = input.length();
         }
     if (input.at(0) == '-')
         input.erase(input.begin());
@@ -49,17 +49,19 @@ int main(int argc, char* argv[]) {
     while (!fin.eof()) {
         getline(fin, buff, ' ');
         buff = TidyString(buff);
-        wordC++;
 
-        if ((mapP = contentTable.find(buff)) != contentTable.end())
-            mapP->second++;
-        else
-            contentTable.insert(pair<string, double>(buff, 1.0));
+        if (!buff.empty()) {
+            wordC++;
+            if ((mapP = contentTable.find(buff)) != contentTable.end())
+                mapP->second++;
+            else
+                contentTable.insert(pair<string, double>(buff, 1.0));
+        }
     }
 
     while (!contentTable.empty()) {
         mapP = findMaxVal(&contentTable);
-        fout << mapP->first << ", " << mapP->second << ", " << mapP->second/wordC * 100 << endl;
+        fout << mapP->first << ", " << mapP->second << ", " << mapP->second/wordC * 100 << "%" << endl;
         contentTable.erase(mapP);
     }
 
