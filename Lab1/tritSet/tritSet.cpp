d951aaef1c76668f3c85d28032a747f2a2ec9cf3
Eistern <zulind@inbox.ru>
2018-10-21 18:04:23 +0700

    Added bitOperations into tritSet class, added test for access to const tritSet

--- Lab1/tritSet/tritSet.cpp
+++ Lab1/tritSet/tritSet.cpp
@@ -7,7 +7,7 @@ tritSet::placeHolder tritSet::operator[] (unsigned int index) {
 Trit tritSet::operator[](unsigned int index) const {
     if (index >= this->length())
         return Trit::UNKNOWN;
-    return getTrit(this->_set[index / 16], index % 16);
+    return getTrit(this->_set[index / (sizeof(size_t) * 4)], index % (sizeof(size_t) * 4));
 }
 
 tritSet tritSet::operator& (tritSet &unit2) {
@@ -17,15 +17,15 @@ tritSet tritSet::operator& (tritSet &unit2) {
     Trit left, right;
     Trit null = Trit::UNKNOWN;
     unsigned int i;
-    for (i = 0; i < min * 16; ++i) {
-        left = getTrit(this->_set[i / 16], i % 16);
-        right = getTrit(unit2._set[i / 16], i % 16);
-        setTrit(_new[i / 16], i % 16, left & right);
+    for (i = 0; i < min * (sizeof(size_t) * 4); ++i) {
+        left = getTrit(this->_set[i / (sizeof(size_t) * 4)], i % (sizeof(size_t) * 4));
+        right = getTrit(unit2._set[i / (sizeof(size_t) * 4)], i % (sizeof(size_t) * 4));
+        setTrit(_new[i / (sizeof(size_t) * 4)], i % (sizeof(size_t) * 4), left & right);
     }
     std::vector<unsigned int> maxV = unit2._set.size() == max ? unit2._set : this->_set;
-    for (i; i < max * 16; ++i) {
-        left = getTrit(maxV[i / 16], i % 16);
-        setTrit(_new[i / 16], i % 16, left & null);
+    for (i; i < max * (sizeof(size_t) * 4); ++i) {
+        left = getTrit(maxV[i / (sizeof(size_t) * 4)], i % (sizeof(size_t) * 4));
+        setTrit(_new[i / (sizeof(size_t) * 4)], i % (sizeof(size_t) * 4), left & null);
     }
     return tritSet(_new);
 }
@@ -37,15 +37,15 @@ tritSet tritSet::operator| (tritSet &unit2) {
     Trit left, right;
     Trit null = Trit::UNKNOWN;
     unsigned int i;
-    for (i = 0; i < min * 16; ++i) {
-        left = getTrit(this->_set[i / 16], i % 16);
-        right = getTrit(unit2._set[i / 16], i % 16);
-        setTrit(_new[i / 16], i % 16, left | right);
+    for (i = 0; i < min * (sizeof(size_t) * 4); ++i) {
+        left = getTrit(this->_set[i / (sizeof(size_t) * 4)], i % (sizeof(size_t) * 4));
+        right = getTrit(unit2._set[i / (sizeof(size_t) * 4)], i % (sizeof(size_t) * 4));
+        setTrit(_new[i / (sizeof(size_t) * 4)], i % (sizeof(size_t) * 4), left | right);
     }
     std::vector<unsigned int> maxV = unit2._set.size() == max ? unit2._set : this->_set;
-    for (i; i < max * 16; ++i) {
-        left = getTrit(maxV[i / 16], i % 16);
-        setTrit(_new[i / 16], i % 16, left | null);
+    for (i; i < max * (sizeof(size_t) * 4); ++i) {
+        left = getTrit(maxV[i / (sizeof(size_t) * 4)], i % (sizeof(size_t) * 4));
+        setTrit(_new[i / (sizeof(size_t) * 4)], i % (sizeof(size_t) * 4), left | null);
     }
     return tritSet(_new);
 }
@@ -54,9 +54,9 @@ tritSet tritSet::operator~ () {
     std::vector<unsigned int> _new(this->_set.size());
     Trit buff;
     unsigned int i;
-    for (i = 0; i < this->_set.size() * 16; ++i) {
-        buff = getTrit(this->_set[i / 16], i % 16);
-        setTrit(_new[i / 16], i % 16, ~buff);
+    for (i = 0; i < this->_set.size() * (sizeof(size_t) * 4); ++i) {
+        buff = getTrit(this->_set[i / (sizeof(size_t) * 4)], i % (sizeof(size_t) * 4));
+        setTrit(_new[i / (sizeof(size_t) * 4)], i % (sizeof(size_t) * 4), ~buff);
     }
     return tritSet(_new);
 }
@@ -64,7 +64,7 @@ tritSet tritSet::operator~ () {
 tritSet::placeHolder::operator Trit() const{
     if (_shift > _from->length())
         return Trit::UNKNOWN;
-    return getTrit(_from->_set[_shift / 16], _shift % 16);
+    return getTrit(_from->_set[_shift / (sizeof(size_t) * 4)], _shift % (sizeof(size_t) * 4));
 }
 
 tritSet& tritSet::placeHolder::operator=(Trit input) { // NOLINT
@@ -73,7 +73,7 @@ tritSet& tritSet::placeHolder::operator=(Trit input) { // NOLINT
 
     if (_shift > _from->length())
         _from->_resizeSet(_shift);
-    setTrit(_from->_set[_shift / 16], _shift % 16, input);
+    setTrit(_from->_set[_shift / (sizeof(size_t) * 4)], _shift % (sizeof(size_t) * 4), input);
     return *_from;
 }
 
@@ -82,23 +82,24 @@ std::size_t tritSet::capacity() const{
 }
 
 void tritSet::_resizeSet(unsigned int to) {
-    this->_set.resize(to / 16 + 1);
+    this->_set.resize(to / (sizeof(size_t) * 4) + 1);
 }
 
 void tritSet::shrink() {
-    this->_resizeSet(length() - 1 > _defaultSize * 16 - 1 ? length() - 1 : _defaultSize * 16 - 1);
+    this->_resizeSet(length() - 1 > _defaultSize * (sizeof(size_t) * 4) - 1 ? length() - 1 : _defaultSize * (sizeof(size_t) * 4) - 1);
 }
 
 void tritSet::trim(size_t lastIndex) {
-    if (lastIndex < length()) {
-        for (int i = lastIndex; i < length(); i++)
+    if (lastIndex < (lastIndex / sizeof(size_t) + 1) * sizeof(size_t)) {
+        for (int i = lastIndex; i < (lastIndex / sizeof(size_t) + 1) * sizeof(size_t); i++)
             (*this)[i] = Trit::UNKNOWN;
     }
+    this->_resizeSet(lastIndex);
 }
 
 std::size_t tritSet::length() const{
-    unsigned int index = this->_set.size() * 16 - 1;
-    while (getTrit(this->_set[index / 16], index % 16) == Trit::UNKNOWN && index != 0)
+    unsigned int index = this->_set.size() * (sizeof(size_t) * 4) - 1;
+    while (getTrit(this->_set[index / (sizeof(size_t) * 4)], index % (sizeof(size_t) * 4)) == Trit::UNKNOWN && index != 0)
         --index;
     ++index;
     return index;
@@ -108,7 +109,7 @@ std::size_t tritSet::cardinality(Trit value) const{
     std::size_t result = 0;
     unsigned int to = this->length();
     for (unsigned int i = 0; i < to; ++i)
-        if (getTrit(this->_set[i / 16], i % 16) == value)
+        if (getTrit(this->_set[i / (sizeof(size_t) * 4)], i % (sizeof(size_t) * 4)) == value)
             result++;
 
     return result;
